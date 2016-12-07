ba5f6543e1b3d06031458bcbc436acba25099531
Cs2 Developer <jonathan.sigg@cs2.ch>
2016-12-07 14:14:06 +0100

    src add

---
+++ ext_emconf.php
@@ -0,0 +1,35 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'CS2 Prototype',
+	'description' => 'LFM styles',
+	'category' => 'fe',
+	'shy' => 1,
+	'version' => '0.0.1',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => '',
+	'author_email' => '',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.10-7.6.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"cf48";s:9:"README.md";s:4:"ed2a";s:21:"ext_conf_template.txt";s:4:"44d9";s:12:"ext_icon.gif";s:4:"6759";s:17:"ext_localconf.php";s:4:"e9b1";s:14:"ext_tables.php";s:4:"daf0";s:13:"locallang.xml";s:4:"13b4";s:33:"Migrations/Code/ClassAliasMap.php";s:4:"5ec9";s:39:"Migrations/Code/LegacyClassesForIde.php";s:4:"db1c";s:43:"Tests/Unit/Configuration/BeRegistryTest.php";s:4:"4cc6";s:36:"Tests/Unit/ExtMgm/StatefieldTest.php";s:4:"6765";s:30:"Tests/Unit/Hooks/SetupTest.php";s:4:"1931";s:37:"Tests/Unit/Parser/DummyParserTest.php";s:4:"9859";s:40:"Tests/Unit/Service/DyncssServiceTest.php";s:4:"9288";s:41:"Resources/Public/Icons/lightning_blue.png";s:4:"a7ae";s:40:"Resources/Private/Language/locallang.xlf";s:4:"b340";s:36:"Classes/Configuration/BeRegistry.php";s:4:"0b39";s:43:"Classes/Command/DyncssCommandController.php";s:4:"e6d2";s:29:"Classes/ExtMgm/Statefield.php";s:4:"c482";s:55:"Classes/Hooks/T3libPageRendererRenderPreProcessHook.php";s:4:"29e2";s:34:"Classes/Hooks/T3libTcemainHook.php";s:4:"2554";s:47:"Classes/Hooks/TslibFeContentPostProcAllHook.php";s:4:"f72f";s:55:"Classes/Hooks/Backend/Toolbar/ClearCacheActionsHook.php";s:4:"9f17";s:33:"Classes/Parser/AbstractParser.php";s:4:"15eb";s:30:"Classes/Parser/DummyParser.php";s:4:"4bf8";s:34:"Classes/Parser/ParserInterface.php";s:4:"e514";s:33:"Classes/Service/DyncssService.php";s:4:"4df7";s:40:"Classes/Utilities/ApplicationContext.php";s:4:"267a";s:35:"Documentation/Images/clearcache.png";s:4:"745f";s:34:"Documentation/Images/sourcemap.png";s:4:"6b0d";}',
+);
+
+?>
